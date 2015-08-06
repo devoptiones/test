db2795264e481077c40504abb0dc3802d934c16c
Boris Yankov <borisyankov@gmail.com>
2015-08-06 11:19:21 +0300

    Add initial gh-pages version

--- app/_constants/ActionTypes.js
+++
@@ -1,2 +0,0 @@
-export const FILTER_MARKETS = 'FILTER_MARKETS';
-export const UPDATE_MARKETS = 'UPDATE_MARKETS';
--- app/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import Root from './root';
-
-React.render(<Root/>, document.getElementById('root'));
